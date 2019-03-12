72c885d51c14133757c2b269aed4992531d5e5f0
It's Me <=>
2019-03-12 23:33:09 +0300

    Remove: unnecessary codes

--- src/presentation-forms.ts
+++ src/presentation-forms.ts
@@ -36,134 +36,84 @@ export const arabicForms = {
     "final": "FE94"
   },
   "لا": {
-    "isolated": "FEFB",
     "final": "FEFC"
   },
   "لإ": {
-    "isolated": "FEF9",
     "final": "FEFA"
   },
   "لأ": {
-    "isolated": "FEF7",
     "final": "FEF8"
   },
   "لآ": {
-    "isolated": "FEF5",
     "final": "FEF6"
   },
   "ئ": {
-    "initial": "FE8B",
-    "medial": "FE8C",
     "final": "FE8A"
   },
   "ب": {
-    "initial": "FE91",
-    "medial": "FE92",
     "final": "FE90"
   },
   "ت": {
-    "initial": "FE97",
-    "medial": "FE98",
     "final": "FE96"
   },
   "ث": {
-    "initial": "FE9B",
-    "medial": "FE9C",
     "final": "FE9A"
   },
   "ج": {
-    "initial": "FE9F",
-    "medial": "FEA0",
     "final": "FE9E"
   },
   "ح": {
-    "initial": "FEA3",
-    "medial": "FEA4",
     "final": "FEA2"
   },
   "خ": {
-    "initial": "FEA7",
-    "medial": "FEA8",
     "final": "FEA6"
   },
   "س": {
-    "initial": "FEB3",
-    "medial": "FEB4",
     "final": "FEB2"
   },
   "ش": {
-    "initial": "FEB7",
-    "medial": "FEB8",
     "final": "FEB6"
   },
   "ص": {
-    "initial": "FEBB",
-    "medial": "FEBC",
     "final": "FEBA"
   },
   "ض": {
-    "initial": "FEBF",
-    "medial": "FEC0",
     "final": "FEBE"
   },
   "ط": {
-    "initial": "FEC3",
-    "medial": "FEC4",
     "final": "FEC2"
   },
   "ظ": {
-    "initial": "FEC7",
-    "medial": "FEC8",
     "final": "FEC6"
   },
   "ع": {
-    "initial": "FECB",
-    "medial": "FECC",
     "final": "FECA"
   },
   "غ": {
-    "initial": "FECF",
-    "medial": "FED0",
     "final": "FECE"
   },
   "ف": {
-    "initial": "FED3",
-    "medial": "FED4",
     "final": "FED2"
   },
   "ق": {
-    "initial": "FED7",
-    "medial": "FED8",
     "final": "FED6"
   },
   "ك": {
-    "initial": "FEDB",
-    "medial": "FEDC",
     "final": "FEDA"
   },
   "ل": {
-    "initial": "FEDF",
-    "medial": "FEE0",
     "final": "FEDE"
   },
   "م": {
-    "initial": "FEE3",
-    "medial": "FEE4",
     "final": "FEE2"
   },
   "ن": {
-    "initial": "FEE7",
-    "medial": "FEE8",
     "final": "FEE6"
   },
   "ه": {
-    "initial": "FEEB",
-    "medial": "FEEC",
     "final": "FEEA"
   },
   "ي": {
-    "initial": "FEF3",
-    "medial": "FEF4",
     "final": "FEF2"
   }
 }
